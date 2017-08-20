852cc967d60e837154059157598d66f67a97023b
wenxiaofu <18627659723@163.com>
2017-08-20 21:26:41 +0800

    first commit

---
+++ src/main/Httpclient.go
@@ -0,0 +1,25 @@
+﻿package main
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"log"
+)
+func Rsp_web(w http.ResponseWriter,r *http.Request) {
+	r.ParseForm() //解析参数
+	fmt.Println(r.Form)//服务端打印客户端上传的信息
+	fmt.Println("path",r.URL.Path)
+	for k,v := range r.Form{
+		fmt.Println("key",k)
+		fmt.Println("val",strings.Join(v,""))
+	}
+	fmt.Fprintf(w,"hello astadd")
+	
+}
+func main(){
+	http.HandleFunc("/",Rsp_web)//设置访问路由
+	err := http.ListenAndServe(":9090",nil)
+	if err != nil {
+		log.Fatal("listenandserve: ",err)
+	}
+}
